dc476da4a81c1337dc294f3210d138767b9c5fa4
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2021-10-17 15:45:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ player_y = 10;
 block_image_width = 30;
 block_image_height = 30;
 
+erase_image_width = 50;
+erase_image_height = 50;
+
+
 
 var player_object= "";
 var block_image_object= "";
@@ -40,111 +44,180 @@ function new_image(get_image)
 	canvas.add(block_image_object);
 
 	});
+
+}
+
+function erase_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+		erase_image_object = Img;
+	
+		block_image_object.scaleToWidth(erase_image_width);
+		block_image_object.scaleToHeight(erase_image_height);
+		block_image_object.set({
+		top:player_y,
+		left:player_x
+		});
+		canvas.add(block_image_object);
+	
+		});
 }
 
 window.addEventListener("keydown", my_keydown);
+
 function my_keydown(e)
 {
-keyPressed=e.keyCode;
- console.log(keyPressed); 
-if (e.shiftkey==true && keyPressed=='80')
+keyPressed = e.keyCode;
+console.log(keyPressed);
+if(e.shiftKey == true && keyPressed == '80')
 {
-   console.log("P and Shift key pressed together");
-  block_image_width=block_image_width+10;
-block_image_height=block_image_height+10;
-document.getElementById("current_width").innerHTML=block_image_width;
-document.getElementById("current_height").innerHTML=block_image_height;
+	console.log("p and shift pressed together");
+	block_image_width = block_image_width + 10;
+	block_image_height = block_image_height + 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;	
 }
-
-if (e.shiftkey==true && keyPressed=='77')
+if(e.shiftKey && keyPressed == '77')
 {
-   console.log("M and Shift key pressed together");
-  block_image_width=block_image_width-10;
-block_image_height=block_image_height-10;
-document.getElementById("current_width").innerHTML=block_image_width;
-document.getElementById("current_height").innerHTML=block_image_height;
+	console.log("m and shift pressed together");
+	block_image_width = block_image_width - 10;
+	block_image_height = block_image_height - 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;
 }
 
-if (keyPressed=='38')
+	if(keyPressed == '38')
+	{
+		up();
+		console.log("up");
+	}
+
+	if(keyPressed == '40')
+	{
+		down();
+		console.log("down");
+	}
+
+	if(keyPressed == '37')
+	{
+		left();
+		console.log("left");
+	}
+
+	if(keyPressed == '39')
+	{
+		right();
+		console.log("right");
+	}
+
+	if(keyPressed == '87')
+	{
+		new_image('wall.jpg'); 
+		console.log("w");
+	}
+	if(keyPressed == '71')
+	{
+		new_image('ground.png'); 
+		console.log("g");
+	}
+
+	if(keyPressed == '89')
+	{
+		new_image('yellow_wall.png'); 
+		console.log("y");
+	}
+
+	if (keyPressed== '84')
 {
-    up();
-    console.log("up");
+	new_image('trunk.jpg');
+	console.log("T");
 }
-if (keyPressed=='40')
+
+if (keyPressed== '82')
 {
-    down();
-    console.log("down");
+	new_image('roof.jpg');
+	console.log("R");	
 }
-
-if (keyPressed=='39')
+if(keyPressed == '85')
 {
-    right();
-    console.log("right");
+	new_image('unique.png');
+	console.log('U');
 }
 
-if (keyPressed=='37')
+if(keyPressed == '76')
 {
-    left();
-    console.log("left");
+	new_image('light_green.png');
+	console.log('L');
+}
+if(keyPressed == '67')
+{
+	new_image('images.jpg');
+	console.log('C');
+}
+if(keyPressed == '68')
+{
+	new_image('dark_green.png');
+	console.log('D');
+}
+if(keyPressed == '76')
+{
+	new_image('light_green.png');
+	console.log('L');
+}
+if ( keyPressed == '69')
+{
+	new_image('Screenshot (786).png');
+	console.log('E');
 }
 
 
-
 }
-
 function up()
 {
-   if(player_y>=0)
-   {
-       player_y=player_y-block_image_height;
-       console.log("block_image_height="+block_image_height);
-       console.log("when Up arrow is pressed, X="+player_x+"Y="+player_y);
-       canvas.remove(player_object);
-       player_update();
-   } 
-   
+	if(player_y >=0)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
 
 function down()
 {
-    if(player_y<=500)
-    {
-        player_y=player_y+block_image_height;
-        console.log("block_image_height="+block_image_height);
-        console.log("when down arrow is pressed, X="+player_x+"Y="+player_y);
-        canvas.remove(player_object);
-        player_update();
-    } 
-    
+	if(player_y <=1000)
+	{
+		player_y = player_y + block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Down arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
 
 function left()
 {
-    if(player_x>0)
-   {
-       player_x=player_x-block_image_width;
-       console.log("block_image_height="+block_image_height);
-       console.log("when left arrow is pressed, X="+player_x+"Y="+player_y);
-       canvas.remove(player_object);
-       player_update();
-   } 
-   
+	if(player_x >0)
+	{
+		player_x = player_x - block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Left arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
 
 function right()
 {
-    if(player_x<=850)
-   {
-       player_x=player_x+block_image_width;
-       console.log("block_image_width="+block_image_width);
-       console.log("when right arrow is pressed, X="+player_x+"Y="+player_y);
-       canvas.remove(player_object);
-       player_update();
-   } 
-   
+	if(player_x <=1200)
+	{
+		player_x = player_x + block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Right arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
-
-
-
 
 
